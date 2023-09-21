30c6063ea3841a6dd00198f8fd7470982f87b4c9
Kevin Santiago Mendes <mbr.ark@gmail.com>
2023-09-21 20:03:29 -0300

    .

--- overlay/js/init.js
+++ overlay/js/init.js
@@ -83,7 +83,7 @@ Mopi2 = {
         autoHide: 1, 
         tooltips: 1, 
         toast: 1, 
-        Lang: "KR", 
+        Lang: "EN",
         fTime: 'DS-Digital',
         fTarget: 'Segoe UI',
         fRPS: 'Roboto Condensed',
